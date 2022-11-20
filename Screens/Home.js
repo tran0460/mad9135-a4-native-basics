ee756b796c4b5d8096f8f01aba54c375f419c3ad
tran0460 <tran0460@algonquinlive.com>
2022-11-20 18:51:46 -0500

    add content for home page

--- Screens/Home.js
+++ Screens/Home.js
@@ -4,7 +4,10 @@ import React from "react";
 const Home = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <Text>Welcome to The Ghibli Library</Text>
+      <Text>
+        The Ghibli Library offers you a list of popular studio Ghibli movies!
+      </Text>
     </View>
   );
 };
