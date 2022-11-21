90492b70050e699e27fff822caf80e514f6f374f
tran0460 <tran0460@algonquinlive.com>
2022-11-20 20:16:48 -0500

    create card component

--- Components/Card.js
+++ Components/Card.js
@@ -1,15 +1,20 @@
-import { View, Text } from "react-native";
+import { Text, Image } from "react-native";
 import React from "react";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const Card = ({ data }) => {
   return (
-    <View>
+    <TouchableOpacity>
       <Text>*****</Text>
+      <Image
+        style={{ width: 300, height: 300 }}
+        source={{ uri: data.movie_banner }}
+      />
       <Text>{data.title}</Text>
       <Text>{data.director}</Text>
       <Text>{data.original_title_romanised}</Text>
       <Text>{data.release_date}</Text>
-    </View>
+    </TouchableOpacity>
   );
 };
 
