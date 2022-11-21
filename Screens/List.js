ce9f0a9d40168899b385f4ccad6f85a785dec93d
tran0460 <tran0460@algonquinlive.com>
2022-11-20 20:16:55 -0500

    create List screen

--- Screens/List.js
+++ Screens/List.js
@@ -1,11 +1,19 @@
-import { View, Text } from "react-native";
-import React from "react";
-
+import { SafeAreaView, ScrollView, FlatList } from "react-native";
+import { useCallback } from "react";
+import { useAppContext } from "../AppContext";
+import Card from "../Components/Card";
 const List = () => {
+  const { data } = useAppContext();
+  const renderListItem = useCallback(
+    ({ item }) => {
+      return <Card data={item} key={item.id} />;
+    },
+    [data]
+  );
   return (
-    <View>
-      <Text>List</Text>
-    </View>
+    <SafeAreaView>
+      <FlatList data={data} renderItem={renderListItem} />
+    </SafeAreaView>
   );
 };
 
