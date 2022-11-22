c649f73989236c4fb8d9231d0dba6767b5bab000
tran0460 <tran0460@algonquinlive.com>
2022-11-21 22:52:24 -0500

    styling

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,15 +1,32 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Welcome to The Ghibli Library</Text>
-      <Text>
+    <View style={styles.screenContainer}>
+      <Image
+        style={{ width: "100%", height: 300, marginBottom: 8 }}
+        source={require("../assets/ghibli-image.jpeg")}
+      />
+      <Text style={styles.title}>Welcome to The Ghibli Library</Text>
+      <Text style={styles.title}>
         The Ghibli Library offers you a list of popular studio Ghibli movies!
       </Text>
     </View>
   );
 };
-
+const styles = StyleSheet.create({
+  screenContainer: {
+    backgroundColor: "#fff",
+    height: "100%",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 25,
+    lineHeight: 30,
+    color: "#111",
+    textAlign: "center",
+    marginVertical: 8,
+  },
+});
 export default Home;
